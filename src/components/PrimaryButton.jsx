c35199e25a9a202ea071ec03957985fec1626fb4
Nazik <shmnazik79@gmail.com>
2024-11-22 13:40:16 +0530

    Primary Button adjusments done

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -1,21 +1,15 @@
-import React from 'react';
+import React from "react";
 
 function PrimaryButton({ hasIcon, iconSrc, text, onClick }) {
   return (
-    <div className="flex flex-col items-center space-y-2">
-      <button
-        className="px-6 py-3 rounded-lg text-white bg-[#391031] hover:bg-[#4a1340] flex items-center space-x-2"
-        onClick={onClick}
-      >
-        {/* Conditionally render icon if hasIcon is true */}
-        {hasIcon && (
-          <img src={iconSrc} alt="Icon" className="w-6 h-6" />
-        )}
-        <span>{text}</span>
-      </button>
-    </div>
+    <button
+      className="gap-3 px-6 py-3 rounded-2xl text-white text-[20px] bg-[#391031] hover:bg-[#4a1340] flex items-center"
+      onClick={onClick}
+    >
+      {hasIcon && <img src={iconSrc} alt="Icon" className="w-3" />}
+      <span>{text}</span>
+    </button>
   );
 }
 
 export default PrimaryButton;
-
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import PrimaryButton from "../components/PrimaryButton";
+import downloadIcon from "../assets/icons/download_icon.png";
 
 function Login() {
-  return <div><PrimaryButton hasIcon={false} text="Login"/></div>;
+  return (
+    <div>
+      <PrimaryButton hasIcon={true} text="Login" iconSrc={downloadIcon} />
+    </div>
+  );
 }
 
 export default Login;
