e6bbe633c6709dafc631a111dc4439684c8627c8
Nazik <shmnazik79@gmail.com>
2024-11-23 23:41:04 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import PrimaryButton from "../components/PrimaryButton";
 import downloadIcon from "../assets/icons/download_icon.png";
-import PrimaryInputField from "../components/PrimaryInputField";
 
 function Login() {
   return (
     <div>
       <PrimaryButton hasIcon={true} text="Login" iconSrc={downloadIcon} />
-      <PrimaryInputField />
     </div>
   );
 }
