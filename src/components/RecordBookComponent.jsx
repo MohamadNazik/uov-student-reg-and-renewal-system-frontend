9168247d734720f864fbc657cef7615002117d4a
maleesha <maleesharathnayake840@gmail.com>
2024-12-13 23:28:31 +0530

    complete

--- src/components/RecordBookComponent.jsx
+++ src/components/RecordBookComponent.jsx
@@ -7,10 +7,10 @@ function RecordBookComponent() {
   return (
     <div className="flex flex-col gap-5 xl:flex-row xl:gap-14">
       {/* Front view */}
-      <div className="w-[200px] sm:w-[390px] bg-[#E13923] h-[300px] sm:h-[500px] flex justify-center items-center overflow-hidden relative">
-        <div className="w-[150px] sm:w-[300px] bg-[#E13923] h-[250px] sm:h-[450px] border-2  border-customBlue flex flex-col justify-start items-center overflow-hidden relative">
-          <img src={uni_logo} alt="University Logo" className="w-[50px] sm:w-[120px] h-auto mt-3 sm:mt-5" />
-          <h3 className="text-customBlue text-[12px] sm:text-[20px] font-inter font-bold mt-4 sm:mt-8 leading-tight "
+      <div className="w-[250px] sm:w-[390px] bg-[#E13923] h-[350px] sm:h-[500px] flex justify-center items-center overflow-hidden relative">
+        <div className="w-[200px] sm:w-[300px] bg-[#E13923] h-[300px] sm:h-[450px] border-2  border-customBlue flex flex-col justify-start items-center overflow-hidden relative">
+          <img src={uni_logo} alt="University Logo" className="w-[50px] sm:w-[120px] h-auto mt-4 sm:mt-5" />
+          <h3 className="text-customBlue text-[12px] sm:text-[20px] font-inter font-bold mt-6 sm:mt-8 leading-tight "
             style={{
               textShadow: `
                1px 1px 0 white, 
@@ -37,7 +37,7 @@ function RecordBookComponent() {
                 
               `,
           }}>Sri Lanka</h3>
-          <h3 className="text-customBlue text-[16px] sm:text-[28px] text-center  font-inter font-bold uppercase mt-6 sm:mt-8 leading-tight" style={{
+          <h3 className="text-customBlue text-[16px] sm:text-[28px] text-center  font-inter font-bold uppercase mt-8 sm:mt-8 leading-tight" style={{
             textShadow: `
                 1px 1px 0 white, 
                 -1px -1px 0 white, 
@@ -49,7 +49,7 @@ function RecordBookComponent() {
                 0 -1px 0 white
               `,
           }}>Student's Record Book</h3>
-          <h3 className="text-customBlue text-[8px] sm:text-[18px] text-center  font-inter font-bold uppercase mt-[55px] sm:mt-[80px] sm:pt-2 leading-tight " style={{
+          <h3 className="text-customBlue text-[8px] sm:text-[18px] text-center  font-inter font-bold uppercase mt-20 sm:mt-[80px] sm:pt-2 leading-tight " style={{
             textShadow: `
                 1px 1px 0 white, 
                 -1px -1px 0 white, 
@@ -66,42 +66,42 @@ function RecordBookComponent() {
 
 
       {/* Back view */}
-      <div className="w-[200px] sm:w-[390px]  bg-white h-[300px] sm:h-[500px] flex justify-center items-center overflow-hidden relative">
-        <div className="w-[150px] sm:w-[300px] bg-white h-[250px] sm:h-[450px] border-2  border-black   flex flex-col justify-start items-center overflow-hidden relative">
-          <div class="w-[75px] h-[15px] sm:w-[120px] sm:h-[40px] sm:text-[12px] border  border-black text-[8px] ml-auto mr-[10px] mt-2">
+      <div className="w-[250px] sm:w-[390px]  bg-white h-[350px] sm:h-[500px] flex justify-center items-center overflow-hidden relative">
+        <div className="w-[200px] sm:w-[300px] bg-white h-[300px] sm:h-[450px] border-2  border-black   flex flex-col justify-start items-center overflow-hidden relative">
+          <div class="w-[85px] h-[20px] sm:w-[120px] sm:h-[40px] sm:text-[12px] border  border-black text-[10px] ml-auto mr-[10px] mt-2">
             <p class="text-center mt-[2px] font-bold">
               Registration No:
             </p>
           </div>
-          <div class="w-[75px] h-[15px] sm:w-[120px] sm:h-[40px] border border-black text-[8px] sm:text-[12px] ml-auto mr-[10px]">
+          <div class="w-[85px] h-[20px] sm:w-[120px] sm:h-[40px] border border-black text-[10px] sm:text-[12px] ml-auto mr-[10px]">
             <p class="text-center mt-[2px]  font-bold">
               2020/CT/000
             </p>
           </div>
-          <div class="text-left  text-[9px] sm:text-[12px] ml-1 mt-2 sm:mr-[40px] sm:mt-10 sm:space-y-2">
+          <div class="text-left  text-[9px] sm:text-[12px] ml-1 mt-5 sm:mr-[40px] sm:mt-10 space-y-1 sm:space-y-3">
             <h4>
               Name With Initials
-              <span class="inline mx-auto  ml-1 sm:ml-[43px]">:</span>
-              <span class="font-bold ml-1  sm:ml-2">J. Doe</span>
+              <span class="inline mx-auto  ml-9 sm:ml-[43px]">:</span>
+              <span class="font-bold ml-3  sm:ml-2">J. Doe</span>
             </h4>
             <h4>
               Name Donated By Initials
-              <span class="inline ml-7">:</span>
+              <span class="inline ml-[10px]">:</span>
               <span class="font-bold ml-3 sm:ml-2">John Doe</span>
             </h4>
             <h4>
               National Identity Card No
-              <span class="inline ml-11">:</span>
+              <span class="inline ml-2 ">:</span>
               <span class="font-bold ml-2 sm:ml-2">123456789012</span>
             </h4>
             <h4>
               Date of Birth
-              <span class="inline ml-1 sm:ml-[75px]">:</span>
+              <span class="inline ml-[60px] sm:ml-[75px]">:</span>
               <span class="font-bold  ml-3 sm:ml-2">01.01.2001</span>
             </h4>
           </div>
 
-          <div className="flex justify-center items-center mt-4 sm:mt-8">
+          <div className="flex justify-center items-center mt-8 sm:mt-8">
             <img
               src={student_img}
               alt="Student Image"
@@ -109,8 +109,8 @@ function RecordBookComponent() {
             />
           </div>
 
-          <div class="flex items-center text-left text-[9px] sm:text-[12px] ml-[5px] mt-5 sm:mr-[70px] sm:mt-10">
-            Signature of the student<span class="ml-1 sm:ml-[15px]">:</span>
+          <div class="flex items-center text-left text-[9px] sm:text-[12px] mr-7 mt-8 sm:mr-[70px] sm:mt-10">
+            Signature of the student<span class="ml-3 sm:ml-[15px]">:</span>
             <img src={holder_signature} alt="signature" class="w-[25px] sm:w-[50px] h-auto ml-2" />
           </div>
         </div>
